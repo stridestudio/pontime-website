a28dd7ebc73b239d3815d0ed36ac4f5321dd9513
WA Wilffert <wawilffert@WAs-MacBook-Pro.local>
2026-04-05 09:50:02 +0200

    Add /test diagnostic page

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'system-ui, sans-serif' }}>
+      <h1>Hallo vanaf Pontime</h1>
+      <p>Als je dit ziet, werkt de basis.</p>
+    </div>
+  )
+}
